cac0c01a0cd3b16662954bbb7a9522f268373263
Tommalieh <muhammedtommalieh@gmail.com>
2020-06-09 15:13:26 +0300

    test

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const server = require('./src/server.js');
 console.log(process.env.MONGODB_URI);
 console.log(process.env.PORT);
+console.log(process.env.CALLBACK_URI);
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
